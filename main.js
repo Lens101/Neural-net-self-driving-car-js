42e51624e4f8334b2d83446a6054cb48c678df5a
Robert Harbord <robertharbord@hotmail.com>
2024-04-08 15:48:22 +0100

    Add traffic

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ canvas.width = 200;
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50);
-
+const traffic = [
+  new Car(road.getLaneCenter(1), -100, 30, 50),
+  new Car(road.getLaneCenter(0), -100, 30, 50),
+];
 animate();
 
 function animate() {
@@ -20,6 +23,7 @@ function animate() {
 
   road.draw(ctx);
   car.draw(ctx);
+  //traffic.draw(ctx);
   //calls animate 30x / sec
   requestAnimationFrame(animate);
 }
